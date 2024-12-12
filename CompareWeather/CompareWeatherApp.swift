55d307a7602b8fba96c154b6f666fbd445350304
Santos Pardo <santosapr@ext.inditex.com>
2024-12-12 20:32:08 +0100

    Initial Commit

---
+++ CompareWeather/CompareWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompareWeatherApp.swift
+//  CompareWeather
+//
+//  Created by Santos Ángel Pardo Ramos on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CompareWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
